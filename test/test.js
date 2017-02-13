d409f995f6916065d54e3039cd1a476160908d70
node-ist <node_ist@massmediagroup.pro>
2017-02-13 12:00:13 +0200

    add new tests

--- test/test.js
+++ test/test.js
@@ -1,7 +1,18 @@
-describe('index page test', function() {
-  it('button should be exist', function() {
+describe('index page', function() {
+  var hasClass = function (element, cls) {
+    return element.getAttribute('class').then(function (classes) {
+      return classes.split(' ').indexOf(cls) !== -1;
+    });
+  };
+  it('button should has class active', function() {
     browser.get('http://localhost:3000/build/');
-    expect(element(by.css('.btn')).isPresent()).toBe(false);
-    
+    var btn = element(by.css('.btn'));
+    expect(hasClass(btn, 'active')).toBe(true);
+  });
+  
+  it('svg should be exist', function() {
+    browser.get('http://localhost:3000/build/');
+    var svg = element(by.css('.nvd3-svg'));    
+    expect(svg.isDisplayed());    
   });
 });
